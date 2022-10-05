73d66bd39bf12a9f53092024b6c85124e6467fc5
Paul Liu <bitspadorg@gmail.com>
2022-10-05 16:44:19 -0400

    update

--- src/app/components/privacy/privacy.component.ts
+++ src/app/components/privacy/privacy.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { LocalStorage } from '@ngx-pwa/local-storage';
 
 @Component({
   selector: 'app-privacy',
   templateUrl: './privacy.component.html',
   styleUrls: ['./privacy.component.scss']
 })
-export class PrivacyComponent {}
+export class PrivacyComponent {
+  lan = 'en';
+  constructor(private localSt: LocalStorage) {
+    this.lan = localStorage.getItem('_lan') || 'en';
+    alert(this.lan)
+  }
+}
