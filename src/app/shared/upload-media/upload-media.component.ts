30710e03fc2bd3ee6cb23ee80166effae95be036
Paul Liu <bitspadorg@gmail.com>
2023-01-26 18:32:36 -0500

    update

--- src/app/shared/upload-media/upload-media.component.ts
+++ src/app/shared/upload-media/upload-media.component.ts
@@ -1,5 +1,8 @@
 
 import { Component, OnInit, ViewChild, EventEmitter, Input, ElementRef, Output } from '@angular/core';
+import { SELECT_PANEL_INDENT_PADDING_X } from '@angular/material/select';
+import { imgModule } from '@syncfusion/ej2-angular-richtexteditor';
+import { exit } from 'process';
 import { UploadService, DocType } from '../../services/upload.service';
 
 @Component({
@@ -36,6 +39,7 @@ export class UploadMediaComponent implements OnInit {
   isChecked(image) {
     return (this.checkedImages.indexOf(image) >= 0);
   }
+
   FieldsChange(image, values: any) {
     const checked = values.currentTarget.checked;
     if (checked) {
@@ -59,11 +63,30 @@ export class UploadMediaComponent implements OnInit {
 
   fileChangeEvent(e: File[]) {
     if (!e) { return; }
+    this.errMsg = '';
     this.uploadSuccess = false;/* DXACF */
     this.url = '';
     const file = e[0];
+
+
+    if(file.size > 262144) {
+      this.errMsg = 'Image file size exceeds 256K bytes.';
+      return;
+    }
+
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+
+    img.onload  = () => {
+      if(img.width != 320 || img.height != 200) {
+        this.errMsg = 'Image size must be 320 x 200 pixels !';
+        return;
+      }
+    
+
     const fileName = file.name;
     const fileType = file.type;
+    
     this.uploadService.applyPresignedUrl(fileName, fileType, DocType.PRODUCT, this.productId).subscribe(
       (ret: any) => {
         const signedUrl = ret.signed_request;
@@ -79,5 +102,9 @@ export class UploadMediaComponent implements OnInit {
       },
       error => this.errMsg = 'Error happened during apply presigned url.'
     );
+    
+    }
+
   }
+
 }
