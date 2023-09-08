22c514bb3ee6d9758d6ce7bf7dd44b8bd7db2064
Ken <kenin.qiu@gmail.com>
2023-09-08 14:08:20 -0400

    f

--- src/app/wallet/components/multisig/dashboard/home/queue/queue.component.ts
+++ src/app/wallet/components/multisig/dashboard/home/queue/queue.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
 import { MultisigService } from 'src/app/services/multisig.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { MultisigService } from 'src/app/services/multisig.service';
   templateUrl: './queue.component.html',
   styleUrls: ['./queue.component.scss']
 })
-export class QueueComponent implements OnInit {
+export class QueueComponent implements OnInit, OnChanges {
 
   pageSize: number = 10;
   pageNum: number = 0;
@@ -15,6 +15,10 @@ export class QueueComponent implements OnInit {
   constructor(private multisigServ: MultisigService) {}
 
   ngOnInit(): void {
+    this.loadQueue();
+  }
+
+  loadQueue() {
     this.multisigServ.getTransactionQueues(this.multisigwallet.address, this.pageSize, this.pageNum).subscribe(
       (ret: any) => {
         if(ret.success) {
@@ -25,4 +29,10 @@ export class QueueComponent implements OnInit {
     );
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.multisigwallet && changes.multisigwallet.currentValue) {
+      this.multisigwallet = changes.multisigwallet.currentValue;
+      this.loadQueue();
+    }
+  }
 }
