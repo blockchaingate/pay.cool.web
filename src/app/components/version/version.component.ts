7aebcbf1031245b971867cfd33d641dd1a028a4c
ken <kenin.qiu@gmail.com>
2024-04-25 10:20:03 -0400

    update parent

--- src/app/components/version/version.component.ts
+++ src/app/components/version/version.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { version } from '../../../environments/version';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-version',
   templateUrl: './version.component.html',
@@ -8,10 +8,12 @@ import { version } from '../../../environments/version';
 })
 export class VersionComponent implements OnInit {
   version: string;
+  production: boolean;
   constructor() { }
 
   ngOnInit(): void {
     this.version = version;
+    this.production = environment.production;
   }
 
 }
